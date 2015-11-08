5c15dad6228d71a5fb43e7995cf35a6845190635
freyconner24 <frey.conner24@gmail.com>
2015-11-07 21:47:59 -0800

    Created and hooked up all Monitor syscalls/servercalls

--- nachos-csci402/code/userprog/lock_syscalls.cc
+++ nachos-csci402/code/userprog/lock_syscalls.cc
@@ -55,8 +55,6 @@ string getFromServer(PacketHeader &pktHdr, MailHeader &mailHdr) {
 }
 
 int CreateLock_sys(int vaddr, int size, int appendNum) {
-	// currentThread->space->locksLock->Acquire(); //CL: acquire kernelLock so that no other thread is running on kernel mode
-
 	char* name = new char[size + 1]; //allocate new char array
 	name[size] = '\0'; //end the char array with a null character
 
@@ -90,47 +88,130 @@ int CreateLock_sys(int vaddr, int size, int appendNum) {
     cout << "currentLockIndex: " << currentLockIndex << endl;
 	//DEBUG('a', "Lock has number %d and name %s\n", currentLockIndex, buffer);
 	//DEBUG('l',"    Lock::Lock number: %d || name: %s created by %s\n", currentLockIndex, currentThread->space->userLocks[currentLockIndex].userLock->getName(), currentThread->getName());
-	// currentThread->space->locksLock->Release(); //release kernel lock
 	return currentLockIndex;
 }
 
-void Acquire_sys(int index) {
-	// currentThread->space->locksLock->Acquire();
-
-	//DEBUG('a', "Lock  number %d and name %s\n", index, currentThread->space->userLocks[index].userLock->getName());
-	//DEBUG('l',"    Lock::Lock number: %d || name:  %s acquired by %s\n", index, currentThread->space->userLocks[index].userLock->getName(), currentThread->getName());
-
-	cout << "+++++++++++++++++++++" << endl;
+void Acquire_sys(int lockIndex) {
 	PacketHeader pktHdr;
 	MailHeader mailHdr;
 
-  sendToServer(pktHdr, mailHdr, "L A ", "", index, -1);
+	sendToServer(pktHdr, mailHdr, "L A ", "", lockIndex, -1);
 
 	string receivedString = getFromServer(pktHdr, mailHdr);
 
     cout << "Acquire::receivedString: " << receivedString << endl;
 
-	// Lock* userLock = currentThread->space->userLocks[index].userLock;
+	// Lock* userLock = currentThread->space->userLocks[lockIndex].userLock;
 	// if(userLock->lockStatus != userLock->FREE) {
 	// 	updateProcessThreadCounts(currentThread->space, SLEEP);
 	// }
 	// currentThread->space->locksLock->Release();//release kernel lock
-	// currentThread->space->userLocks[index].userLock->Acquire(); // acquire userlock at index
+	// currentThread->space->userLocks[lockIndex].userLock->Acquire(); // acquire userlock at lockIndex
 	// currentThread->space->locksLock->Release();
 }
 
-void Release_sys(int index) {
-	// currentThread->space->locksLock->Acquire(); // CL: acquire kernelLock so that no other thread is running on kernel mode
-
-	// DEBUG('l',"    Lock::Lock number: %d || and name: %s released by %s\n", index, currentThread->space->userLocks[index].userLock->getName(), currentThread->getName());
+void Release_sys(int lockIndex) {
 	PacketHeader pktHdr;
 	MailHeader mailHdr;
 
-    sendToServer(pktHdr, mailHdr, "L R ", "", index, -1);
+    sendToServer(pktHdr, mailHdr, "L R ", "", lockIndex, -1);
 
 	string receivedString = getFromServer(pktHdr, mailHdr);
 
     cout << "Release::receivedString: " << receivedString << endl;
+
+	// currentThread->space->locksLock->Release();//release kernel lock
+	// currentThread->space->userLocks[lockIndex].userLock->Release(); // release userlock at index
+	// // destroys lock if lock is free and delete flag is true
+	// if(currentThread->space->userLocks[lockIndex].userLock->lockStatus == currentThread->space->userLocks[index].userLock->FREE && currentThread->space->userLocks[index].deleteFlag == TRUE) {
+	// 	DEBUG('l'," Lock  number %d  and name %s is destroyed by %s \n", lockIndex, currentThread->space->userLocks[index].userLock->getName(), currentThread->getName());
+	// 	currentThread->space->userLocks[lockIndex].isDeleted = TRUE;
+	// 	delete currentThread->space->userLocks[lockIndex].userLock;
+	// }
+}
+
+void DestroyLock_sys(int lockIndex) {
+	PacketHeader pktHdr;
+	MailHeader mailHdr;
+
+    sendToServer(pktHdr, mailHdr, "L D ", "", lockIndex, -1);
+
+	string receivedString = getFromServer(pktHdr, mailHdr);
+
+    cout << "DestroyLock::receivedString: " << receivedString << endl;
+}
+
+// ++++++++++++++++++++ MONITORS ++++++++++++++++++++++++
+
+int CreateMonitor_sys(int vaddr, int size, int appendNum) {
+	char* name = new char[size + 1]; //allocate new char array
+	name[size] = '\0'; //end the char array with a null character
+
+	if (copyin(vaddr, size, name) == -1){
+		DEBUG('l',"%s"," COPYIN FAILED\n");
+		delete[] name;
+		currentThread->space->locksLock->Release();
+		return -1;
+	}; //copy contents of the virtual addr (ReadRegister(4)) to the name
+
+	// set attributes of new lock
+
+    PacketHeader pktHdr;
+	MailHeader mailHdr;
+
+    sendToServer(pktHdr, mailHdr, "M C ", name, -1, -1);
+
+    string receivedString = getFromServer(pktHdr, mailHdr);
+    stringstream ss;
+    ss << receivedString;
+
+    int currentLockIndex = -1;
+    ss >> currentLockIndex;
+
+    if(currentLockIndex == -1) {
+        cout << "Client::currentLockIndex == -1" << endl;
+        interrupt->Halt();
+    }
+
+	currentThread->space->lockCount += 1;
+    cout << "currentLockIndex: " << currentLockIndex << endl;
+	//DEBUG('a', "Lock has number %d and name %s\n", currentLockIndex, buffer);
+	//DEBUG('l',"    Lock::Lock number: %d || name: %s created by %s\n", currentLockIndex, currentThread->space->userLocks[currentLockIndex].userLock->getName(), currentThread->getName());
+	return currentLockIndex;
+}
+
+void GetMonitor_sys(int monitorIndex) {
+	//DEBUG('a', "Lock  number %d and name %s\n", monitorIndex, currentThread->space->userLocks[index].userLock->getName());
+	//DEBUG('l',"    Lock::Lock number: %d || name:  %s acquired by %s\n", monitorIndex, currentThread->space->userLocks[index].userLock->getName(), currentThread->getName());
+
+	PacketHeader pktHdr;
+	MailHeader mailHdr;
+
+	sendToServer(pktHdr, mailHdr, "M G ", "", monitorIndex, -1);
+
+	string receivedString = getFromServer(pktHdr, mailHdr);
+
+    cout << "Client::GetMonitor::receivedString: " << receivedString << endl;
+
+	// Lock* userLock = currentThread->space->userLocks[monitorIndex].userLock;
+	// if(userLock->lockStatus != userLock->FREE) {
+	// 	updateProcessThreadCounts(currentThread->space, SLEEP);
+	// }
+	// currentThread->space->locksLock->Release();//release kernel lock
+	// currentThread->space->userLocks[monitorIndex].userLock->Acquire(); // acquire userlock at index
+	// currentThread->space->locksLock->Release();
+}
+
+void SetMonitor_sys(int monitorIndex) {
+	// DEBUG('l',"    Lock::Lock number: %d || and name: %s released by %s\n", monitorIndex, currentThread->space->userLocks[index].userLock->getName(), currentThread->getName());
+	PacketHeader pktHdr;
+	MailHeader mailHdr;
+
+    sendToServer(pktHdr, mailHdr, "M S ", "", monitorIndex, -1);
+
+	string receivedString = getFromServer(pktHdr, mailHdr);
+
+    cout << "Client::SetMonitor::receivedString: " << receivedString << endl;
 
 	// currentThread->space->locksLock->Release();//release kernel lock
 	// currentThread->space->userLocks[index].userLock->Release(); // release userlock at index
@@ -142,16 +223,13 @@ void Release_sys(int index) {
 	// }
 }
 
-void DestroyLock_sys(int index) {
-	// currentThread->space->locksLock->Acquire();; // CL: acquire locksLock so that no other thread is running on kernel mode
+void DestroyMonitor_sys(int monitorIndex) {
 	PacketHeader pktHdr;
 	MailHeader mailHdr;
 
-    sendToServer(pktHdr, mailHdr, "L D ", "", index, -1);
+    sendToServer(pktHdr, mailHdr, "M D ", "", monitorIndex, -1);
 
 	string receivedString = getFromServer(pktHdr, mailHdr);
 
-    cout << "DestroyLock::receivedString: " << receivedString << endl;
-
-	// currentThread->space->locksLock->Release();//release kernel lock
+    cout << "Client::DestroyMonitor::receivedString: " << receivedString << endl;
 }
